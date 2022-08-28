a27cc6cfa659416875a2d91daec6211b580a6469
Chandler Clevenger <Chandler888@icloud.com>
2022-08-27 21:41:43 -0400

    Refactor to leverage react-draggable more

---
+++ src/componets/router.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+import Draggable from "react-draggable";
+
+export default function Router({ onStop, id, x, y, start, size }: any) {
+  const draggableRef = useRef<Draggable>(null);
+  return (
+    <>
+      <Draggable
+        ref={draggableRef}
+        onStart={start}
+        onStop={onStop}
+        bounds="#board"
+        defaultPosition={{ x: 0, y: 0 }}
+        handle=".router"
+      >
+        <img
+          id={id}
+          draggable={false}
+          className="absolute router"
+          src="./images/pixil-7.png"
+          alt="Placeable Router"
+          style={{ transform: `translate(${x}px, ${y}px)`, width: size }}
+        />
+      </Draggable>
+    </>
+  );
+}
